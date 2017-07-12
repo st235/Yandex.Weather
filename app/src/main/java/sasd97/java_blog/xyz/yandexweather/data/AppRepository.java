36d6289c255a0668461220647de372630ae2cb01
alexander <st235@yandex.ru>
2017-07-12 13:19:54 +0300

    Add retrolambda/retrofit/rx dependencies

---
+++ app/src/main/java/sasd97/java_blog/xyz/yandexweather/data/AppRepository.java
@@ -0,0 +1,8 @@
+package sasd97.java_blog.xyz.yandexweather.data;
+
+/**
+ * Created by alexander on 12/07/2017.
+ */
+
+public interface AppRepository {
+}
---
+++ app/src/main/java/sasd97/java_blog/xyz/yandexweather/data/net/WeatherApi.java
@@ -0,0 +1,8 @@
+package sasd97.java_blog.xyz.yandexweather.data.net;
+
+/**
+ * Created by alexander on 12/07/2017.
+ */
+
+public interface WeatherApi {
+}
